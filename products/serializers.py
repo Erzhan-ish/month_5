86d07d9531cbbb60d7e73d35204576c33c45d32b
Erzhan-ish <isenbekoverzan1@gmail.com>
2025-01-16 00:40:00 +0600

    ls_4

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Category, SearchWord, Review
+from rest_framework.exceptions import ValidationError
 
 class ProductItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,3 +40,24 @@ class ProductSerializer(serializers.ModelSerializer):
             return product.category.name
         except:
             return 'No Category'
+
+class ProductValidateSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True, min_length=3, max_length=255)
+    text = serializers.CharField(required=False)
+    price = serializers.FloatField(min_value=10, max_value=1000000)
+    is_active = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
+    search_words = serializers.ListField(child=serializers.IntegerField(min_value=1))
+
+    def validate_catgory_id(self, category_id):
+        try:
+            Category.objects.get(id=category_id)
+        except:
+            raise ValidationError('Category does not exist!')
+        return category_id
+
+    def validate_search_words(self, search_words):
+        search_words_db = SearchWord.objects.filter(name__in=search_words)
+        if len(search_words) != len(search_words_db):
+            raise ValidationError('SearchWords does not exist!')
+        return search_words
